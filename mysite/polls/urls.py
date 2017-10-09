1f40aac10ebfbaa7e42fcd20f53c4b2e91efe8c3
luzushka <igonnabanu@gmail.com>
2017-10-09 23:21:28 +0300

    added a template and synced it

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
 urlpatterns = [
+    # /polls/
     url(r'^$',views.index,name='index'),
+
+    # /music/71/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
     ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.template import loader
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Album
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    all_albums= Album.objects.all()
+    template=loader.get_template('polls/index.html')
+    context={
+        'all_albums': all_albums,
+        }
+    return HttpResponse(template.render(context, request))
+
+def detail (request,album_id):
+    return HttpResponse("<h2>Details for album id: "+str(album_id) +"</h2>")
